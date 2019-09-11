ef4ad94eeb1a527d2aa659bee9105c2544b0e22f
banobepascal <banobepascal@gmail.com>
2019-09-12 00:34:50 +0300

    adding svgs.js

--- js/svgs.js
+++ js/svgs.js
@@ -1,19 +1,19 @@
-var more = document.querySelector('.fun');
-more.addEventListener('click', runEvent);
+var more = document.querySelector(".fun");
+more.addEventListener("click", runEvent);
 
 function runEvent() {
-	$(function(){
-		$('.fun').hide();
-		$('.fun1').show();
-	});
+  $(function() {
+    $(".fun").hide();
+    $(".fun1").show();
+  });
 }
 
-var less = document.querySelector('.fun1');
-less.addEventListener('click', runEvent1);
+var less = document.querySelector(".fun1");
+less.addEventListener("click", runEvent1);
 
 function runEvent1() {
-	$(function(){
-		$('.fun1').hide();
-		$('.fun').show();
-	});
+  $(function() {
+    $(".fun1").hide();
+    $(".fun").show();
+  });
 }
